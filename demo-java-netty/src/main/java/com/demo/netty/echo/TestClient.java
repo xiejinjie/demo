bfb0cdeeee494bb52baaaff3766478c476934cad
xiejinjie <544482194@qq.com>
2022-11-27 17:11:58 +0800

    update demo

--- demo-java-netty/src/main/java/com/demo/netty/echo/TestClient.java
+++ demo-java-netty/src/main/java/com/demo/netty/echo/TestClient.java
@@ -1,22 +1,19 @@
-package com.demo.netty;
+package com.demo.netty.echo;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class Client {
+public class TestClient {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 8080);
         OutputStream os = socket.getOutputStream();
         os.write("hello".getBytes());
         InputStream is = socket.getInputStream();
-        byte[] bytes = new byte[1024];
-        int len;
-        while ((len = is.read(bytes)) != -1) {
-            System.out.print(new String(bytes, 0, len, "utf-8"));
-        }
-        os.close();
-
+        byte[] buf = new byte[1024];
+        int read = is.read(buf);
+        System.out.println(new String(buf, 0, read));
+        socket.close();
     }
 }
