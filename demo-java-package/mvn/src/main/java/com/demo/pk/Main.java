69995259ee3a6784d75e30d77a73f435f2973b64
xiejinjie <544482194@qq.com>
2023-02-13 10:36:04 +0800

    add demo

---
+++ demo-java-package/mvn/src/main/java/com/demo/pk/Main.java
@@ -0,0 +1,12 @@
+package com.demo.pk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
