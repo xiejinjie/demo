1037eab9123c7e60e20086fcd30fc7c0f08006ac
xiejinjie <544482194@qq.com>
2024-10-23 11:23:24 +0800

    feat: add nacos config demo

---
+++ component-system/nacos/demo-nacos-spring-cloud-config-yml/src/main/java/com/demo/nacos/NacosConfigApplication.java
@@ -0,0 +1,17 @@
+package com.demo.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * NacosConfigApplication
+ *
+ * @author xiejinjie
+ * @date 2024/10/23
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
---
+++ component-system/nacos/demo-nacos-spring-cloud-config-yml/src/main/java/com/demo/nacos/config/ConfigController.java
@@ -0,0 +1,26 @@
+package com.demo.nacos.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ConfigController
+ *
+ * @author xiejinjie
+ * @date 2024/10/23
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
