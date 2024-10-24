fbd68c0af3667ca5776760d40157e480e90ff6b2
xiejinjie <544482194@qq.com>
2024-10-24 18:53:21 +0800

    ✨feat: add demo logback get property from nacos

---
+++ java-component/logback/demo-logback-config-nacos/src/main/java/com/demo/nacos/Application.java
@@ -0,0 +1,36 @@
+package com.demo.nacos;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * NacosConfigApplication
+ *
+ * @author xiejinjie
+ * @date 2024/10/23
+ */
+@SpringBootApplication
+public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        // 示例日志记录
+        logger.info("Application started.");
+        try {
+            // 模拟一些操作
+            throw new RuntimeException("Simulated exception");
+        } catch (Exception e) {
+            logger.error("An error occurred", e);
+        }
+
+        Executors.newScheduledThreadPool(1).scheduleWithFixedDelay(
+                () -> logger.info("Application is running..."),
+                2, 2, TimeUnit.SECONDS);
+    }
+}
---
+++ java-component/logback/demo-logback-config-nacos/src/main/java/com/demo/nacos/PropertyPrinter.java
@@ -0,0 +1,34 @@
+package com.demo.nacos;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * ConfigController
+ *
+ * @author xiejinjie
+ * @date 2024/10/23
+ */
+@Component
+public class PropertyPrinter implements ApplicationRunner {
+    private static final Logger logger = LoggerFactory.getLogger(PropertyPrinter.class);
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        logger.info("logging.config: " + env.getProperty("logging.config"));
+        logger.info("logging.file.name: " + env.getProperty("logging.file.name"));
+    }
+}
