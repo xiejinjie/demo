d63b10ddfe34578bbdae712f1597d0234135da04
xiejinjie <544482194@qq.com>
2024-10-24 08:04:36 +0800

    ✨feat: 添加demo logback springboot输入日志到mysql

--- java-component/logback/demo-logback-spring-mysql/src/main/java/com/demo/logback/Application.java
+++ java-component/logback/demo-logback-spring-mysql/src/main/java/com/demo/logback/Application.java
@@ -2,6 +2,8 @@ package com.demo.logback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Main
@@ -9,10 +11,12 @@ import org.slf4j.LoggerFactory;
  * @author xiejinjie
  * @date 2024/10/23
  */
-public class Main {
-	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+@SpringBootApplication
+public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
         // 示例日志记录
         logger.info("Application started.");
         try {
